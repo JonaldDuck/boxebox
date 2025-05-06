f280e1e9d5c51a65502b1dc6c68b1d2b065d348c
Jttanner <jonttanner@gmail.com>
2025-05-06 12:02:34 -0600

    first attempt at ebox tracking

---
+++ README.md
@@ -0,0 +1,15 @@
+This addon keeps track the contents of your ebox on CatseyeXi for Crystal Warriors.
+
+Each time you check or put something into ebox, the count tracking each item will be updated.
+The amount won't be subtracted from it due to the limitations I know how to deal with (just an ambiguous you obtain message)
+if you access or put anything into the box then it will update with the correct amount again.
+
+It's mostly useful as an audit of what you have.  If you withdraw everything you can check it again with an !box command or put some back in and it will update.
+
+If you have never accessed an item in the ebox it won't know how many you have
+
+To install, copy the boxebox folder into your addons folder
+
+Usage:
+load addon:
+/addon load boxebox 
---
+++ boxebox.lua
@@ -0,0 +1,131 @@
+-- MIT License
+
+-- Copyright (c) 2025 Jonald Duck
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+addon.name      = "boxebox";
+addon.author    = "JonaldDuck";
+addon.version   = "1.0.0";
+addon.desc      = "Track the contents of your ebox in a file";
+addon.link      = "https://github.com/JonaldDuck/boxebox";
+
+require("common");
+
+local eboxFileName = "ebox.txt";
+
+local path = ('%s\\config\\addons\\%s'):fmt(AshitaCore:GetInstallPath(), 'boxebox');
+
+local boxDict = {};
+
+
+print("-- boxebox --")
+
+-- example ebox moessage
+-- Ephemeral Box : I have 2 Ifritite. (Goldsmithing    Rock)
+
+ashita.events.register('text_in', 'text_in_cb', function (e)
+    --really gross I should really brush up on regex and not do hard coded substring indecies lol
+    if e ~= nil and e.message ~= nil then
+        if string.find(e.message, "Ephemeral Box : I have") then
+            --update boxDicts with parsed item and how many we have of it
+
+            --strip out the parenthesis at the end
+            local itemString = string.gsub(e.message, "%([^()]*%)", "")
+            --strip out the characters before the item of ebox message and the period at the end
+            itemString = string.sub(itemString, 23, string.len(itemString)-3)
+
+            --strip out the number of items we have of it
+            local amount = itemString:match("(%d+) (.+)")
+
+            local itemName = string.sub(itemString, string.len(amount)+2, string.len(itemString))
+            itemName = itemName:match("^%s*(.-)%s*$")
+
+            boxDict[itemName] = amount
+            saveBoxData()
+        end
+        if string.find(e.message, "You store") then
+            --strip out you store
+            local itemString = string.sub(e.message, 11, string.len(e.message))
+
+            local amount = itemString:match("%(([^%)]+)%)")
+            amount = amount:match("(%d+)")
+
+            local itemName = itemString:match("^([^x]+)x%d+")
+            itemName = itemName:match("^%s*(.-)%s*$")
+
+            boxDict[itemName] = amount
+            saveBoxData()
+        end
+    end
+    
+end);
+
+function loadBoxData()
+    if ashita.fs.exists(path) then
+        print("Loading ebox data from file.")
+        local file, errorString = io.open(path .. "\\" .. eboxFileName, "r");
+        if file ~= nil then
+            for line in file:lines() do
+                local key, value = line:match("([^=]+) = ([^=]+)")
+                if key ~= nil and value ~= nil then
+                    boxDict[key] = value;
+                end
+            end
+            
+
+            print("Ebox data found and loaded.  Access the file in the config/addons/boxebox folder to see what is there.")
+            io.close(file);
+        end
+        
+        if boxDict == nil then
+            print("No box data found, a new file will be created to track the box.");
+        end
+        
+    end
+end
+
+loadBoxData()
+
+function saveBoxData()
+    if not ashita.fs.exists(path) then
+        ashita.fs.create_directory(path);
+    end
+    local file, errorString = io.open( path .. "\\" .. eboxFileName, "w+" );
+
+    if file ~=nil then
+        for key, value in pairs(boxDict) do
+            file:write(key .. " = " .. value .. "\n")
+        end
+    end
+    io.close(file);
+end
+
+ashita.events.register("command", "command_callback1", function (e)
+    local args = e.command:args();
+    if (#args == 0 or args[1] ~= "/boxebox") then
+        return;
+    else
+        e.blocked = true;
+        if (args[2] == "reset") then
+            print("To reset your box contents, delete the ebox.json file in the config/addons/boxebox folder.");
+            print("Note that if you have logged into another character, this will not track the differences between them or if you've played on multiple.");
+        end
+    end
+end);
